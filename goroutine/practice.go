dce2d38f20b8dacc6fe9a827bb1e41cd00902b36
DamianYongjae <klick214@gmail.com>
2020-04-07 10:58:28 -0400

    job scrapper done

---
+++ goroutine/practice.go
@@ -0,0 +1,52 @@
+package practice
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request Failed")
+
+type requestResult struct {
+	url    string
+	status string
+}
+
+func practice() {
+	results := make(map[string]string)
+	c := make(chan requestResult)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- requestResult) {
+	status := "OK"
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
+
+}
